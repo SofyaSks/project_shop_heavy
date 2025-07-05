f38509cdd3c756366f70e2dc4d179e55020b7be4
Sofya Surovneva <ssurovneva@mail.ru>
2025-07-05 14:56:44 +0300

    models, admin registration, migrations

--- heavyaura/main/admin.py
+++ heavyaura/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . models import Product, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # значения, которые заполняются автоматически
+    prepopulated_fields = {'slug': ('name', )}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated', 'discount']
+    # то, по чему будет фильтрация
+    list_filter = ['available', 'created', 'updated']
+    # то , что можем изменить после создания
+    list_editable = ['price', 'available', 'discount']
+    prepopulated_fields = {'slug': ('name', )}
+    
