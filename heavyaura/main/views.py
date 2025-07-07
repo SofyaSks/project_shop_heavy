8de74adfa3741ac1b0910a311abd210eb66804db
Sofya Surovneva <ssurovneva@mail.ru>
2025-07-07 19:02:46 +0300

    pagination

--- heavyaura/main/views.py
+++ heavyaura/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from . models import Product, Category
 
 # 1 09 28
@@ -21,16 +22,24 @@ def product_detail(request, slug):
 
 
 def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available = True)
+    # отвечает за то, сколько карточек на странице
+    paginator = Paginator(products, 10)
+    current_page = paginator.page(int(page))
     if category_slug:
         category = get_object_or_404(Category, slug = category_slug)
-        products = products.filter(category = category)
+        paginator = Paginator(products.filter(category=category), 10)
+        # products = products.filter(category = category)
+        current_page = paginator.page(int(page))
 
     return render (request,
                    'main/product/list.html',
                    {'category': category, 
                     'categories': categories,
-                    'products': products}
+                    'products': current_page,
+                    'slug_url': category_slug}
                    )
+
